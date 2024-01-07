91f204469969e75789f951ca63b53ae138a3725c
Daly <dalyna64@gmail.com>
2024-01-07 21:38:33 +0100

    start SideBare

--- src/Layout/SidePanel/SideBare/SideBare.jsx
+++ src/Layout/SidePanel/SideBare/SideBare.jsx
@@ -2,7 +2,35 @@ import "./SideBare.css";
 const SideBare = () => {
   return (
     <div className="SideBare">
-      <p>side bare</p>
+      {/* <p>side bare</p> */}
+
+      <img src="/src/assets/icons/three-points.svg" alt="three-points" />
+      <img className="Icon" src="/src/assets/icons/logo.svg" alt="logo" />
+      <img
+        className="Icon"
+        src="/src/assets/icons/projects.svg"
+        alt="projects"
+      />
+      <img className="Icon" src="/src/assets/icons/user.svg" alt="user" />
+      <img
+        className="Icon"
+        src="/src/assets/icons/calendar.svg"
+        alt="calendar"
+      />
+      <img className="Icon" src="/src/assets/icons/statics.svg" alt="statics" />
+      <img
+        className="Icon"
+        src="/src/assets/icons/downloads.svg"
+        alt="downloads"
+      />
+      <img className="Icon" src="/src/assets/icons/plans.svg" alt="plans" />
+      <img
+        className="Icon"
+        src="/src/assets/icons/settings.svg"
+        alt="settings"
+      />
+      <div className="Spacer" />
+      <img className="Icon" src="/src/assets/icons/exit.svg" alt="exit" />
     </div>
   );
 };
